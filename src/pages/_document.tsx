38f5156b8c354d902d3dd0dc7f3888d655999bd1
D <207247077+design-rrr@users.noreply.github.com>
2026-06-24 14:59:32 +0200

    Update favicon and manifest links in _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,9 +7,12 @@ export default class Document extends NextDocument {
     return (
       <Html dir="auto">
         <Head>
-          <link rel="icon" type="image/png" href={asset("/favicon.png")} />
-          <link rel="manifest" href={asset("/manifest.json")} />
-          <link rel="apple-touch-icon" href={asset("/favicon.png")} />
+          <link rel="icon" type="image/png" href={asset("/favicon-96x96.png")} sizes="96x96" />
+          <link rel="icon" type="image/svg+xml" href={asset("/favicon.svg")} />
+          <link rel="shortcut icon" href={asset("/favicon.ico")} />
+          <link rel="apple-touch-icon" sizes="180x180" href={asset("/apple-touch-icon.png")} />
+          <meta name="apple-mobile-web-app-title" content="HABLA" />
+          <link rel="manifest" href={asset("/site.webmanifest")} />
           <meta name="apple-mobile-web-app-status-bar" content="#151220" />
           <meta name="theme-color" content="#151220" />
         </Head>

5c8207e6c4b3a7cc09f04f981296a3476c005ab4
D <207247077+design-rrr@users.noreply.github.com>
2026-06-24 14:49:50 +0200

    Add SITE_URL constant and siteUrl function

--- src/lib/basepath.ts
+++ src/lib/basepath.ts
@@ -1,6 +1,11 @@
 const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || "";
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://design-rrr.github.io/habla.news";
 
 export function asset(path: string) {
   if (path.startsWith("http")) return path;
   return `${BASE_PATH}${path}`;
+}
+
+export function siteUrl(path: string = "") {
+  return `${SITE_URL}${path}`;
 }
